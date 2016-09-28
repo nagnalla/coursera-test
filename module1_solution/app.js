9b0971795b8878e12e69277ca5ea082fb1a9e800
nnala <nallan.politico@gmail.com>
2016-09-27 21:23:01 -0400

    updating exclamations

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -8,11 +8,11 @@
     $scope.perfomLunchCheck = function() {
       //$scope.inputvalues="see there";
       if(! $scope.inputvalues || 0 === $scope.inputValues){
-        $scope.message = "Please Enter Data First";
+        $scope.message = "Please Enter Data First !";
       }
       var splits = $scope.inputvalues.split(",");
       if(splits.length ==0){
-        $scope.message ="Can't be empty";
+        $scope.message ="Can't be empty !";
       }
       if(splits.length >0 && splits.length <= 3){
         $scope.message ="Enjoy !";
