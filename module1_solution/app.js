5a7307456d1431a7919ee4fed54fac69d365e9c6
nnala <nallan.politico@gmail.com>
2016-09-27 21:17:50 -0400

    assignment1 solution

---
+++ module1_solution/app.js
@@ -0,0 +1,25 @@
+(function (){
+  'use strict';
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope){
+    //$scope.inputvalues="sampledata";
+    $scope.perfomLunchCheck = function() {
+      //$scope.inputvalues="see there";
+      if(! $scope.inputvalues || 0 === $scope.inputValues){
+        $scope.message = "Please Enter Data First";
+      }
+      var splits = $scope.inputvalues.split(",");
+      if(splits.length ==0){
+        $scope.message ="Can't be empty";
+      }
+      if(splits.length >0 && splits.length <= 3){
+        $scope.message ="Enjoy !";
+      }else{
+        $scope.message ="Too Much !";
+      }
+    }
+  }
+
+})();
